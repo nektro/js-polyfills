eaafd636a465ed5c3c93406411ff64bc25064ca7
Sean D <nektromc@gmail.com>
2017-03-03 11:13:36 -0500

    Update Array.prototype.powerSet.js
    turn return value from array to iterable

--- src/Array.prototype.powerSet.js
+++ src/Array.prototype.powerSet.js
@@ -1,12 +1,9 @@
 /** Returns the [power set](https://en.wikipedia.org/wiki/Power_set) of this Array
  */
-Array.prototype.powerSet = function() {
-    var res = new Array();
+Array.prototype.powerSet = function*() {
     var max = Math.pow(2, this.length);
     for (var i = 0; i < max; i++) {
         var that = this;
-        var sub = i.toString(2).split('').map(function(x,i) { return x === '1' ? that[i] : undefined; }).clean(undefined).toString();
-        res.push(sub);
+        yield i.toString(2).split('').map(function(x,i) { return x === '1' ? that[i] : undefined; }).clean(undefined).toString();
     }
-    return res;
 };
