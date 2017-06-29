1a8ab2567dd63ada5e01a8d4c3db2bc6e0a7095d
Nektro <nektromc@gmail.com>
2017-06-28 20:05:26 -0400

    use native functions in indexOf

--- src/HTMLCollection.prototype.indexOf.js
+++ src/HTMLCollection.prototype.indexOf.js
@@ -2,14 +2,6 @@
  */
 if (!('indexOf' in HTMLCollection.prototype)) {
     HTMLCollection.prototype.indexOf = function(el) {
-        if (!(el instanceof Element)) {
-            return;
-        }
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] === el) {
-                return i;
-            }
-        }
-        return -1;
+        return Array.from(this).indexOf(el);
     };
 }
